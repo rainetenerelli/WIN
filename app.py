04feb830feaeab66427a52ace009c78e39e765ab
Christine Lam <somebody@wellesley.edu>
2020-09-22 22:34:49 -0400

    added app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+'''
+Project: Wushu Inventory Ninja
+Authors: Elaney Cheng, Christine Lam, Raine Tenerelli, Eugenia Zhang
+Course: CS304 Fall T1 2020
+'''
+from flask import (Flask, render_template, make_response, url_for, request,
+                   redirect, flash, session, send_from_directory, jsonify)
+
+@app.route('/')
+def index():
+    return render_template('main.html',title='Hello')
+
+
+@app.before_first_request
+def init_db():
+    dbi.cache_cnf()
+    dbi.use('uwushu_db')
+
+if __name__ == '__main__':
+    import sys, os
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+        assert(port>1024)
+    else:
+        port = os.getuid()
+    app.debug = True
+    app.run('0.0.0.0',port)
